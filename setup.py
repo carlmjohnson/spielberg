c98327f92ab525bccbca7d3ae5b75bed375b2af2
Carl Johnson <cmjohnson@pbs.org>
2016-01-12 13:44:45 -0500

    Create a blank project

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Spielberg',
+    version='0.0.1',
+    packages=find_packages(),
+    package_dir={},
+    classifiers=[
+    ],
+    include_package_data=True,
+    zip_safe=False)
